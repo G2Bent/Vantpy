46042e13197679942c37773d51d2c8f4f37d22c3
origin <944921374@qq.com>
2018-06-04 23:49:32 +0800

    add pymysql

--- Base/BrowserDriver.py
+++ Base/BrowserDriver.py
@@ -21,37 +21,37 @@ class BrowserDriver(object):
     def openbrowser(self,driver):
         #读取配置文件
         file_path = os.path.dirname(os.getcwd())
-        name_path = file_path + '\conf\config.yaml'
+        name_path = file_path + '\yaml\\browser.yaml'
         with open(name_path, 'r') as f:
             temp = yaml.load(f.read())
 
         # 获取配置文件属性
         brow = temp['brwserType']['browserName']
         browser = brow
-        logger.info("You had select %s browser." % browser)
+        logger.info("选择的浏览器为: %s 浏览器" % browser)
         ur = temp['testUrl']['URL']
         url = ur
-        logger.info("The test url is: %s" % url)
+        logger.info("打开的URL为: %s" % url)
 
         if browser == "Firefox":
             driver = webdriver.Firefox()
-            logger.info("Starting firefox browser.")
+            logger.info("启动火狐浏览器")
         elif browser == "Chrome":
             driver = webdriver.Chrome(self.chrome_driver_path)
-            logger.info("Starting Chrome browser.")
+            logger.info("启动谷歌浏览器")
         elif browser == "IE":
             driver = webdriver.Ie(self.ie_driver_path)
-            logger.info("Starting IE browser.")
+            logger.info("启动IE浏览器")
 
         driver.get(url)
-        logger.info("Open url: %s" % url)
+        logger.info("打开URL: %s" % url)
         driver.maximize_window()
-        logger.info("Maximize the current window.")
+        logger.info("全屏当前窗口")
         driver.implicitly_wait(5)
-        logger.info("Set implicitly wait 5 seconds.")
+        logger.info("设置5秒隐式等待时间")
         return driver
 
     def quit_browser(self):
-        logger.info("Quit the browser.")
+        logger.info("关闭浏览器")
         self.driver.quit()
 
